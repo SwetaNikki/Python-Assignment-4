68c121bf01b5a8ca815824771bb77b5c9e29cdb9
SwetaNikki <56434154+SwetaNikki@users.noreply.github.com>
2020-08-04 12:52:20 +0530

    Create AreaOfTriangle.py
    1.1 Write a Python Program(with class concepts) to find the area of the triangle using the below
    formula.
    area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
    Function to take the length of the sides of triangle from user should be defined in the parent
    class and function to calculate the area should be defined in subclass.

---
+++ AreaOfTriangle.py
@@ -0,0 +1,30 @@
+"""
+1.1 Write a Python Program(with class concepts) to find the area of the triangle using the below
+formula.
+area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
+Function to take the length of the sides of triangle from user should be defined in the parent
+class and function to calculate the area should be defined in subclass.
+
+"""
+import math
+class TakingInput:
+    def __init__(self,a,b,c):
+        self.a = a
+        self.b = b
+        self.c = c
+a = int(input("Enter a value : "))
+b = int(input("Enter b value : "))
+c = int(input("Enter c value : "))
+        
+class AreaOfTriangle():
+    def __init__(self,a,b,c):
+        TakingInput.__init__(self,a,b,c)
+      
+    def area(self):
+        s=(a+b+c)/2
+        area=math.sqrt(s*(s-a)*(s-b)*(s-c))
+        return area
+        
+        
+ob = AreaOfTriangle(a,b,c)
+print("Area of Triangle : {}".format(ob.area()))
